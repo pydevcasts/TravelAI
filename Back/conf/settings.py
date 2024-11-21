f549f082585de0a7ed3d2268249df3f8b013b00f
pydevcasts <siyamak1981@gmail.com>
2024-11-22 00:03:07 +0330

    Edit settings.py

--- Back/conf/settings.py
+++ Back/conf/settings.py
@@ -108,8 +108,8 @@ else:
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "siyamak1981@gmail.com"
-EMAIL_HOST_PASSWORD = "pazg hjyt vsfx mpxm"
+EMAIL_HOST_USER = "pydevcasts@gmail.com"
+EMAIL_HOST_PASSWORD = "*******"
 EMAIL_USE_TLS = True
 
 AUTH_PASSWORD_VALIDATORS = [
