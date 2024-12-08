b3c6e2b72bbb97c15b4958f0416037daf88d502a
pydevcasts <43889064+pydevcasts@users.noreply.github.com>
2024-12-08 13:51:49 +0330

    Revert " fixed some space"

--- Back/tag/models.py
+++ Back/tag/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 
+
+
 class Tag(models.Model):
     name = models.CharField(max_length=50, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -8,3 +10,4 @@ class Tag(models.Model):
 
     def __str__(self) -> str:
         return f"{self.name}"
+    
